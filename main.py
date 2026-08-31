40699ced8050092c5a2a3d7bdff7e2a3e4e05c0b
Ayobamidele Ewetuga <ayobamideleewetuga@gmail.com>
2026-08-31 06:45:05 -0700

    feat: configure CORS middleware with allowed origins and regex for Vercel deployments

--- main.py
+++ main.py
@@ -4,8 +4,27 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from api.db.database import check_database_connection, get_async_db
 from api.utils.responses import success_response
 from api.v1.routes import api_version_one
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://inviteme.name.ng",
+    "https://www.inviteme.name.ng",
+    "http://localhost:3000",
+]
+
+# Regex to match your specific Vercel project deployment subdomains
+vercel_regex = r"^https://inviteme(-[a-zA-Z0-9-]+)?\.vercel\.app$"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_origin_regex=vercel_regex,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_version_one)
 
 
